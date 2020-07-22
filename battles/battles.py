5f738ff352da3cbab6769b7d2a383c465ff3a9cb
blukat <blukatstudios@gmail.com>
2020-07-22 17:49:25 -0400

    Added a script to simulate battles.

---
+++ battles/battles.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import sys
+sys.path.append('../')
+from dice import roll_die
+
+def simulation(attacker, defender):
+  print("Begin battle. Attacker: {} Defender: {}".format(attacker.strength, defender.strength))
+  while attacker.strength > 0 and defender.strength > 0:
+    attacker.strength -= defender.attack()
+    defender.strength -= defender.attack()
+    print("Attacker: {}, Defender:{}".format(attacker.strength, defender.strength))
+  if(attacker.strength > 0):
+    print("Attacker won with {} strength".format(attacker.strength))
+  else:
+    print("Defender won with {} strength".format(defender.strength))
+
+class Army:
+  def __init__(self, strength=100):
+    self.strength = strength
+
+  def attack(self):
+    attack_power = int(self.strength/2)
+    roll = roll_die(max(attack_power, 1))['result']
+    return min(roll, self.strength)
+
+def main():
+  print("{}, {}".format(sys.argv[1], sys.argv[2]))
+
+  if(len(sys.argv) == 3):
+    simulation(Army(int(sys.argv[1])), Army(int(sys.argv[2])))
+  else:
+    simulation(Army(), Army())
+
+main()
