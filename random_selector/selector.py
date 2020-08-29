4b80c9a7ef7f064ad288f04986ea4c1493dfbeab
blukat <blukatstudios@gmail.com>
2020-08-29 19:30:25 -0400

    Refactored selection script to be more unix-y.

--- random_selector/selector.py
+++ random_selector/selector.py
@@ -6,28 +6,36 @@ import sys
 sys.path.append('../')
 from dice import roll_result
 from discord_chat import send_message_to_discord
-AVAILABLE_SLOTS=4
-INTERESTED_PARTIES=[
-  'Jabber',
-  'Axel',
-  'Pigshanks',
-  'Steel',
-  'Bruce'
-]
 
+def get_interested_parties():
+  i = 2
+  interested_parties = []
+  while i < len(sys.argv):
+    interested_parties.append(sys.argv[i])
+    i += 1
+  return interested_parties
+
+def select_x(x, options):
+  selected = []
+  for i in range(x):
+    selection_made = False
+    while not selection_made:
+      roll = roll_result(len(options)-1)
+      selected_option = options[roll]
+      if selected_option not in selected:
+        selected.append(selected_option)
+        selection_made = True
+  return selected
 
 def main():
-  chosen_parties = []
-  output = "Selecting from {}\n".format(INTERESTED_PARTIES)
-  for i in range(AVAILABLE_SLOTS):
-    found = False
-    while not found:
-      choice = roll_result(len(INTERESTED_PARTIES))
-      chosen_party = INTERESTED_PARTIES[int(choice)]
-      if chosen_party not in chosen_parties:
-        chosen_parties.append(chosen_party)
-        output += "Chose {}\n".format(chosen_party)
-        found = True
+  if len(sys.argv) < 3:
+    print("Usage: selector <number of slots> <player names>")
+  available_slots = int(sys.argv[1])
+  interested_players = get_interested_parties()
+  selected_players = select_x(available_slots, interested_players)
+  output = "Selecting from {}\n".format(interested_players)
+  for i in range(len(selected_players)):
+    output += "Chose player {}\n".format(selected_players[i])
   print(output)
   send_message_to_discord(output)
 main()
